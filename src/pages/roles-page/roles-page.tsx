53f5f7b80974dc3311d0e8e06d8666f05e9ee03a
tohird03 <tohirjondoniyorov@gmail.com>
2023-02-13 00:04:20 +0500

    Add roles page ui

---
+++ src/pages/roles-page/roles-page.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { MiniDrawer } from "../../components/sidebar/sidebar";
+import { IPerson } from "../../interfaces/users.interfaces";
+import { getUsers }  from "../../services/api"
+
+export const RolesPage: React.FC = () => {
+    const [users, setUsers] = useState<IPerson[]>([]);
+
+    useEffect(() => {
+      getUsers().then(data => setUsers(data))
+    }, [])
+
+    return <MiniDrawer users={users} />;
+};
